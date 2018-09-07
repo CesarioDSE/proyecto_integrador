16aa7ffb7c14583f4feba1ba1f066d98c7bc0887
Cesario <Cesar.duenas.dse@uteq.edu.mx>
2018-09-07 12:17:09 -0500

    issues resolved

--- code_test.c
+++ code_test.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-
+#define RESULTADO 5
 
 
 float division(float a, float b);
 int suma(int a, int b);
+int resta(int a, int b);
 
 int main()
 {
